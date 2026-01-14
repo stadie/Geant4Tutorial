0343be3043cd49bc2531c24df4fc150895da9ecf
Hartmut Stadie <hartmut.stadie@cern.ch>
2026-01-14 17:16:57 +0100

    change tag

---
+++ setup_docker.sh
@@ -0,0 +1,23 @@
+#setup root
+conda activate geant
+#source /usr/local/root532/bin/thisroot.sh
+# set PATH to geant installation
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+#export G4PATH=/home/student/
+export VGM_INSTALL=/home/student/vgm
+export VGM_SYSTEM=Linux-g++#
+#export LD_LIBRARY_PATH=/usr/lib:/usr/local/geant4/geant4.9.5.p02-install/lib/:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+export G4INSTALL="/home/student/.conda/envs/geant/"
+#source  /usr/share/geant4/env.sh
+#source /usr/local/geant4/geant4.9.5.p02-install//bin/geant4.sh
+#export G4LEVELGAMMADATA=`readlink -e $G4PATH/PhotonEvaporation2.2/`
+#export G4LEDATA=`readlink -e $G4PATH/G4EMLOW6.23/`
+export USE_VGM=1
+
+export LD_LIBRARY_PATH=$VGM_INSTALL/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/student/vmc/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/student/geant4_vmc/lib:$LD_LIBRARY_PATH
+# path to geant4
